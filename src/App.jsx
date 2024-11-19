97ff06f7ea3cb6d79a9abf8a6cce6c0b9be07cd2
Zyad TAIBI <zyadt@MacBook-Air-de-Zyad.local>
2024-11-19 17:26:04 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -25,11 +25,7 @@ const App = () => {
   return (
     <>
       {" "}
-      {isLoading ? (
-        <span> EN cours de chargement...</span>
-      ) : (
-        <div>{data.restaurant.path}</div>
-      )}
+      {isLoading ? <span> EN cours de chargement...</span> : <div>{data}</div>}
     </>
   );
 };
