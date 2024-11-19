e14423ff8fbd86afc23bc38a3b533dbd4432c2c1
Zyad TAIBI <zyadt@MacBook-Air-de-Zyad.local>
2024-11-19 17:27:31 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ const App = () => {
         const response = await axios.get(
           "https://site--backend-deliveroo--cszclskmpcqr.code.run/"
         );
-        console.log(response.data.data);
-        setData(response.data.data);
+        console.log(response);
+        setData(response);
         setIsLoading(false);
       } catch (error) {
         console.log(error.response); // contrairement au error.message d'express
